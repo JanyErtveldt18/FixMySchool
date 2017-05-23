e5376b5b0dc9804fc61a6598b555a2b132f8bea1
Jany Ertveldt <jany.ertveldt@student.ehb.be>
2017-05-23 13:45:45 +0200

    weg doen Bojan

--- js/navigatie.js
+++
@@ -1,8 +0,0 @@
-function openNav() {
-    document.getElementById("mySidenav").style.marginLeft = "0";
-}
-
-function closeNav() {
-    document.getElementById("mySidenav").style.marginLeft = "-100%";
-}
-
