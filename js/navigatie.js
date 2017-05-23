1d78ae64ca4459102317345c0ce1299058231af2
Jany Ertveldt <jany.ertveldt@student.ehb.be>
2017-05-23 13:43:10 +0200

    Jany zijn files

---
+++ js/navigatie.js
@@ -0,0 +1,8 @@
+function openNav() {
+    document.getElementById("mySidenav").style.marginLeft = "0";
+}
+
+function closeNav() {
+    document.getElementById("mySidenav").style.marginLeft = "-100%";
+}
+
