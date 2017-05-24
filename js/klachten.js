30927b4ec7e6f8c08e2de6a11701bca0439f23f7
Axel-IR <axel.iradukunda@student.ehb.be>
2017-05-24 15:24:50 +0200

    klachten

---
+++ js/klachten.js
@@ -0,0 +1,18 @@
+function dropDown(){
+   document.getElementById("dropContent").classList.toggle("show");
+}
+window.onclick = function(event) {
+  if (!event.target.matches('.dropknop')) {
+
+    var dropdowns = document.getElementsByClassName("content");
+      console.log(dropdowns);
+    var i;
+    for (i = 0; i < dropdowns.length; i++) {
+      var openDropdown = dropdowns[i];
+        console.log(openDropdown);
+      if (openDropdown.classList.contains('.show')) {
+        openDropdown.classList.remove('.show');
+      }
+    }
+  }
+}
