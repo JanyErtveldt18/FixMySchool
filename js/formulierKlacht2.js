b9dfdaf51e8b60417966cbf2e545c034f41f1ae6
Dieter Blockmans <dieter.blockmans@gmail.com>
2017-05-29 12:08:24 +0200

    dieter

---
+++ js/formulierKlacht2.js
@@ -0,0 +1,11 @@
+var onderwerp = document.getElementById("onderwerpTekst").value;
+console.log(onderwerp);
+
+
+//function formulierOpslaan(){
+//    if (typeof(Storage) !== "undefined"){
+//        localStorage
+//    }else{
+//        
+//    }
+//}
