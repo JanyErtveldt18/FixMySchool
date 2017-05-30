aa5687ae36f2c729264726e46f57e8373aeb8c35
Jany Ertveldt <jany.ertveldt@student.ehb.be>
2017-05-30 21:18:58 +0200

    ugfezjds

--- js/eigenInzendingen.js
+++ js/eigenInzendingen.js
@@ -2,7 +2,7 @@ var dt = new Date();
 test();
 function test(){
 
-  document.getElementById("mijnklachtenlijst").innerHTML = '<li><span>' + dt.getDate() + "/0" + (dt.getMonth() + 1) + "/" + dt.getFullYear() + '</span>' + localStorage.getItem('hierWordtHetOnderwerpInOpgeslagen') + '<span>'+localStorage.getItem("hierWordtHetGebouwInOpgeslagen")+"."+localStorage.getItem("hierWordtHetLokaalInOpgeslagen")+'</span><img src="../img/Information.svg" alt=""><img src="../img/roodbol.svg" alt=""></li>';
+  document.getElementById("mijnklachtenlijst").innerHTML = '<a href="klachtenbeschrijvingLocalStorage.html"><li><span>' + dt.getDate() + "/0" + (dt.getMonth() + 1) + "/" + dt.getFullYear() + '</span>' + localStorage.getItem('hierWordtHetOnderwerpInOpgeslagen') + '<span>'+localStorage.getItem("hierWordtHetGebouwInOpgeslagen")+"."+localStorage.getItem("hierWordtHetLokaalInOpgeslagen")+'</span><img src="../img/Information.svg" alt=""><img src="../img/roodbol.svg" alt=""></li></a>';
   
   
 }
