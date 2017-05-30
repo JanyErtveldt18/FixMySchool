243d9a213fb3598e7e3a4966ac5e2f4e05defa4e
Jany Ertveldt <jany.ertveldt@student.ehb.be>
2017-05-30 10:28:47 +0200

    push

--- js/klachtenbeschrijvingLocalStorage.js
+++ js/klachtenbeschrijvingLocalStorage.js
@@ -1,10 +1,15 @@
 var dt = new Date();
 var kijkenOfDeLocalStorageLeeg = localStorage.getItem("hierWordtHetOnderwerpInOpgeslagen");
+var kijkenOfDeLocalStorageLeeg2 = localStorage.getItem("hierWordtDeCampusInOpgeslagen");
+var kijkenOfDeLocalStorageLeeg3 =  localStorage.getItem("hierWordtHetGebouwInOpgeslagen");
+var kijkenOfDeLocalStorageLeeg4 =  localStorage.getItem("hierWordtHetLokaalInOpgeslagen");
+var kijkenOfDeLocalStorageLeeg5 =  localStorage.getItem("hierWordtDeBeschrijvingInOpgeslagen");
 
-if (!kijkenOfDeLocalStorageLeeg) {
+
+if (!kijkenOfDeLocalStorageLeeg && !kijkenOfDeLocalStorageLeeg2 && !kijkenOfDeLocalStorageLeeg3 && !kijkenOfDeLocalStorageLeeg4 && !kijkenOfDeLocalStorageLeeg5) {
   console.log("Er is niets in de localStorage opgeslagen, dus er komt geen nieuwe klacht bij!");
   window.location = "klachten.html";
-} else {
+}else {
   console.log("Verder funcities gaan uitgevoerd worden");
   vulBeschrijving();
 }
@@ -13,6 +18,14 @@ if (!kijkenOfDeLocalStorageLeeg) {
 function vulBeschrijving(){
 
  document.getElementById("hierKomtHetOnderwerp").innerHTML = '<h1>' + localStorage.getItem('hierWordtHetOnderwerpInOpgeslagen') + '</h1>';
+  
+ document.getElementById("hierKomtHetGebouw").innerHTML = '<h2>' + localStorage.getItem('hierWordtHetGebouwInOpgeslagen') +"."+ localStorage.getItem('hierWordtHetLokaalInOpgeslagen') +" - Campus "+localStorage.getItem('hierWordtDeCampusInOpgeslagen') + '</h2>';  
+  
+ document.getElementById("hierKomtDeDatum").innerHTML = '<h3>' + + dt.getDate() + "/0" + (dt.getMonth() + 1) + "/" + dt.getFullYear()  + '</h3>';  
+  
+ document.getElementById("hierKomtDeBeschrijving").innerHTML = '<p>' +localStorage.getItem("hierWordtDeBeschrijvingInOpgeslagen")+'</p>';  
+ 
+ 
   
 }
 
